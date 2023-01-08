6523dc469bc46ccbb98dfbd96899e9241c3841ea
Crypto_Vdim <vadimrace@gmail.com>
2023-01-08 11:22:32 -0500

    added missing div in shell code, added standard buisness hours divs and updated all ids

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+//TO-DO
+//2. deploy live server and play around with html code
+//3. add div
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
